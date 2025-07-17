922422766b5f1bddc199063c9276501f348b2d2f
BeomjunCho <chobj555@gmail.com>
2025-07-17 14:20:44 -0700

    Boot scene and logic added.

---
+++ 1333_Template/Assets/Scripts/Boot/BootManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class BootManager : Singleton<BootManager>
+{
+    [Header("Scene Names")]
+    [SerializeField] private string bootSceneName = "BootScene";
+    [SerializeField] private string mainMenuSceneName = "MainMenu";
+
+    private void Start()
+    {
+        Debug.Log("Boot manager starts");
+        var active = SceneManager.GetActiveScene().name;
+        if (active == bootSceneName)
+            SceneManager.LoadScene(mainMenuSceneName);
+        Debug.Log("Boot manager loaded main menu scene");
+    }
+}
